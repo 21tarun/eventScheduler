ae84080d843c9d8defe21d73cb8b6867362b0210
21tarun <tarunuday2000@gmail.com>
2023-02-11 15:47:26 +0530

    event schedular

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ mongoose.set("strictQuery",true)
 
 
 
-// mongoose.connect("mongodb+srv://tarun21:tarun1616@cluster0.h0l8mir.mongodb.net/eventScheduler",{
-//     useNewUrlParser:true
-// })
-// .then(()=>console.log("mongodb connected"))
-// .catch((err)=>console.log(err))
+mongoose.connect("mongodb+srv://tarun21:tarun1616@cluster0.h0l8mir.mongodb.net/eventScheduler",{
+    useNewUrlParser:true
+})
+.then(()=>console.log("mongodb connected"))
+.catch((err)=>console.log(err))
 
 app.use('/',route)
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -14,40 +14,56 @@ router.post('/createEvent',async function(req,res){
     if(!data.text) return res.status(400).send({status:false, message:"event text mandatory"})
     dataForCreate.text=data.text.trim()
     let date= data.year+'-'+data.month+'-'+data.day
-    let time= data.hour+':'+data.minutes+':'+data.seconds
+    let time= data.hour+':'+data.minutes
      
     dateTime=`${date} ${time}`
     
     
-    
-
-    
-    
-
     dataForCreate.dateTime=dateTime
 
     const saveData=await eventModel.create(dataForCreate)
 
     
     res.status(201).send({status:true,message:"event scheduled",data:saveData})
+    let list= await eventModel.find()
+
+    script(list)
 
 
 })
 
-list=[
-    {text:"interview1", dateTime:"2023-02-10 21:12:20"},
-    {text:"interview2", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview3", dateTime:"2023-02-10 21:15:20"},
-    {text:"interview4", dateTime:"2023-02-10 21:56:20"},
-    {text:"interview5", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview6", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview7", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview8", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview9", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview10", dateTime:"2023-02-10 21:13:20"},
-    {text:"interview11", dateTime:"2023-02-10 21:13:20"}
+// list=[
+//     {text:"interview1", dateTime:"2023-02-10 21:12:20"},
+//     {text:"interview2", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview3", dateTime:"2023-02-10 21:15:20"},
+//     {text:"interview4", dateTime:"2023-02-10 21:56:20"},
+//     {text:"interview5", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview6", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview7", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview8", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview9", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview10", dateTime:"2023-02-10 21:13:20"},
+//     {text:"interview11", dateTime:"2023-02-10 21:13:20"}
 
-]
+// ]
+
+const script =function(list){
+    cron.schedule("* * * * *",function(){
+        let dateTime=moment.tz('Asia/Calcutta').format()
+        dateTime=dateTime.replace("+05:30","")
+        dateTime=dateTime.replace("T"," ")
+        dateTime=dateTime.slice(0,16)
+        dateTime=String(dateTime)
+    
+        for(let i=0;i<list.length;i++){
+            
+            if(list[i].dateTime==dateTime){
+                console.log(list[i].text)
+            }
+        }
+    })
+}
+
 
 
 
